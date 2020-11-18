0f2615940cf131a131a52d4e5eb6d300612a5e9f
Oliix74 <70209183+Oliix74@users.noreply.github.com>
2020-11-18 11:50:08 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -3,6 +3,7 @@ var commands = {
     'salut': function() { alert('Bonjour!'); },
     'bonjour': function() { alert('Salut!'); },
     'salut': function() { alert('Salut! comment vas-tu ?'); },
-    'ça va': function() { alert('Bien et vous ?'); }
+    'ça va': function() { alert('Bien et vous ?'); },
+    'est-ce que tu connais les NASCAR': function() { alert('Evidemment, j\'adore Brad Keselowski ');}
 
 }
