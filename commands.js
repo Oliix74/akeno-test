9b6b6b279ec16ac4ba9bd1a87d707ad67c156f7f
Oliix74 <70209183+Oliix74@users.noreply.github.com>
2020-11-18 10:56:26 +0100

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,8 @@
+var commands = {
+
+    'salut': function() { alert('Bonjour!'); },
+    'bonjour': function() { alert('Salut!'); },
+    'salut': function() { alert('Salut! comment vas-tu ?'); },
+    'ça va': function() { alert('Bien et vous ?'); }
+
+}
