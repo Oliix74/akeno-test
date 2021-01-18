6b841ba6d7a0469f701f7e07af3bb768d07bd84d
Oliix74 <70209183+Oliix74@users.noreply.github.com>
2021-01-18 21:50:09 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,14 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.1.1/howler.min.js"></script>
-<script>
-    var sound = new Howl({
-      src: ['https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'],
-      volume: 0.5,
-      onend: function () {
-        alert('Finished!');
-      }
-    });
-    sound.play()
-</script>
 
 window.alert("Hajimemashite !");
 
